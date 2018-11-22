3df420eaa1c7d08dfd83e53669cafc259741280d
Danielle Bequeaith <danielle.rdb@outlook.com>
2018-11-22 00:12:56 +0000

    made routes more RESTful, added show route and a new field in campground schema in v2

--- v2/app.js
+++ v2/app.js
@@ -11,7 +11,8 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -19,7 +20,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Granite Hill", 
-//         image: "https://www.visitmesaverde.com/media/399916/morefield-campground_mesa-verde_0072_1000x667.jpg"
+//         image: "https://www.visitmesaverde.com/media/399916/morefield-campground_mesa-verde_0072_1000x667.jpg",
+//         description: "This is a huge granite hill, no bathrooms.  No water.  Beautiful granite"
 //     }, function(err, campground){
 //         if(err){
 //             console.log(err);
@@ -33,26 +35,29 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX route - show all campgrounds
 app.get("/campgrounds", function(req, res){
     //Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         }
     });
     
     //res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+//CREATE route - add new campground to DB
 app.post("/campgrounds", function(req, res){
     //get data from form
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
     
     //Create a new campground and save to DB
-    var newCampground = {name: name, image: image};
+    var newCampground = {name: name, image: image, description: desc};
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
@@ -62,8 +67,21 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    //find campgroung with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
---
+++ v2/readme.md
@@ -0,0 +1,8 @@
+RESTFUL ROUTES
+
+name    url                 verb    desc
+============================================================================
+INDEX   /campgrounds        GET     Display a list of all campgrounds
+NEW     /campgrounds/new    GET     Displays a form to make a new campground
+CREATE  /campgrounds        POST    Add new campground to DB
+SHOW    /campgrounds/:id    GET     Shows info about one campground
